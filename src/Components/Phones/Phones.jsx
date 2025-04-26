54b3437b885c9bddf3417c25698f51c4beccab36
umme-Tahazzee <tahazzeekaria1@gmail.com>
2025-04-26 12:14:20 +0600

    Explore React Awesome components

--- src/Components/Phones/Phones.jsx
+++ src/Components/Phones/Phones.jsx
@@ -1,40 +1,52 @@
-import axios from 'axios'
-import  { useEffect, useState } from 'react'
-import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
+import { Audio } from "react-loader-spinner";
 
 const Phones = () => {
- const [phones, setPhones] = useState([])
+  const [phones, setPhones] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-   useEffect(()=>{
-       
-         axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
-         .then(data=>{
-            const phoneData = data.data.data;
-            const phonesWithFakeData = phoneData.map(phone=>{
-              const obj = {
-                name : phone.phone_name,
-                price : parseInt(phone.slug.split('-')[1])
-              }
-              return obj;
-            })
-            console.log(phonesWithFakeData);
-       
-            setPhones(phonesWithFakeData)
-         })
-         
-   }, [])
+  useEffect(() => {
+    axios
+      .get("https://openapi.programming-hero.com/api/phones?search=iphone")
+      .then((data) => {
+        const phoneData = data.data.data;
+        const phonesWithFakeData = phoneData.map((phone) => {
+          const obj = {
+            name: phone.phone_name,
+            price: parseInt(phone.slug.split("-")[1]),
+          };
+          return obj;
+        });
+        console.log(phonesWithFakeData);
+        setPhones(phonesWithFakeData);
+        setLoading(false);
+      });
+  }, []);
   return (
-    <div className='p-10'>
-       <h2 className="text-5xl mb-10">Phones: {phones.length}</h2>     
-        <BarChart width={1200} height={400} data={phones}>
-          <Bar dataKey="price" fill="#8884d8" />
-          <XAxis dataKey='name'></XAxis>
-          <YAxis></YAxis>
-          <Tooltip></Tooltip>
-        </BarChart>
-    </div>
-  )
-}
+    <div className="p-10">
+     {loading && <div className="flex items-center text-center justify-center w-full">
+      <Audio
+        height="80"
+        width="80"
+        radius="9"
+        color="green"
+        ariaLabel="loading"
+        wrapperStyle
+        wrapperClass
+      />
+      </div>}
 
-export default Phones
+      <h2 className="text-5xl mb-10">Phones: {phones.length}</h2>
+      <BarChart width={1200} height={400} data={phones}>
+        <Bar dataKey="price" fill="skyblue" />
+        <XAxis dataKey="name"></XAxis>
+        <YAxis></YAxis>
+        <Tooltip></Tooltip>
+      </BarChart>
+    </div>
+  );
+};
+
+export default Phones;
